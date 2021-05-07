2b143368a3c122b75ab388f304a89d2130568385
Cheryl Wan <cherylwan3@gmail.com>
2021-05-06 20:12:16 -0700

    string_txt_processing/10.js

--- small_problems/string_txt_processing/10.js
+++ small_problems/string_txt_processing/10.js
@@ -18,7 +18,7 @@
 // ex
 const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
 
-searchWord('sed', text);
+console.log(searchWord('sed', text));
 // returns
 // "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem 
 // accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
@@ -37,21 +37,27 @@ searchWord('sed', text);
 
 
 function searchWord(word, string) {
-  word = word.toLowerCase()
-  let result = string
+  return string
     .split(' ')
     .map(strWord => {
-      testWord = strWord.toLowerCase();
-      if (testWord === word) {
+      if (strWord.toLowerCase() === word.toLowerCase()) {
         return '**' + strWord.toUpperCase() + '**';
       } 
       return strWord;
     })
     .join(' ');
- console.log(result);
 }
 
-function searchWord(word, text) {
+function searchWord2(word, text) {
   let regex = new RegExp(`\\b${word}\\b`, "gi");
   return text.replace(regex, `**${word.toUpperCase()}**`);
+}
+
+// good solution
+function searchWord3(word, text) {
+  let lowercaseText = text.toLowerCase();
+  let uppercaseWord = word.toUpperCase();
+  let newString = lowercaseText.split(word).join(`**${uppercaseWord}**`);
+
+  return newString;
 }

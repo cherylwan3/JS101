86e2ca67a28056c840fa4f35525b870deee2ce44
Cheryl Wan <cherylwan3@gmail.com>
2021-05-09 15:36:12 -0700

    medium2

--- small_problems/medium2/6.js
+++ small_problems/medium2/6.js
@@ -1,15 +1,111 @@
 /**
  * P---------------------
- * input:
- * output:
+ * input: func (num)
+ * output: num
  * rules:
- * -
+ * - func returns difference btw:
+ *  1) square of the sum of the first count positive integers MINUS
+ *  2) the sum of the squares of the first count positive integers.
  * D------------------
- * -
+ * - sumSquareDifference(3);
+ * numbers = [1, 2, 3]
+ * --------------------------------
+ * - func @difference (num1, num2)
+ *  - num1- num2
+ * -------------------------------
+ * // sum of the first count positive integers
+ * - func @sumArry
+ *  - iterate over @numbers
+ *    - add all the elements together
+ *    - returns total @sum number
+ * ----------------------------
+ * - func @square (num)
+ *  - returns num ** 2
  * A-----------------
- * -
- * -
+ * - function sumSquareDifference(count)
+ * - declare @diffNum1 @diffNum2 @currentSum
+ * 
+ * // for differ
+ * - declare @numbers to and set it to []
+ * - start a loop and iterate from counter of 1 to count
+ *     - append (idx + 1) to numbers
+ * - end loop
+ * -set @currentSum to: call @sumArray on @numbers 
+ * - set @diffNum1 to @currentSum
+ * - set @diffNum1 calling @square on @currentSUm
+ * 
+ * // for diffNum2
+ * - set @squaredNumbers to: 
+ * - start loop: iterate over @numbers array -> 
+ *    - call @square on every element @number
+ *    - return the squared number
+ * - end the loop
+ * - set @diffNum2 to calling @sum on @squaredNumbers
+ *    
+ * //difference
+ *  return diffNum1 - diffNum2
  * 
 */
+/* // my long solution using many functions
+function sumSquareDifference(count) {
+  let numbers = getNumbersArray(count);
+  let diffNum1 = square(sumArray(numbers));
+
+  let squaredNumbers = numbers.map(num => square(num));
+  let diffNum2 = sumArray(squaredNumbers);
+  
+  return diffNum1 - diffNum2;
+}
+
+function getNumbersArray(count) {
+  let numbers = [];
+  for (let counter = 1; counter <= count; counter += 1) {
+    numbers.push(counter)
+  }
+  return numbers;
+}
+
+function sumArray(numArray) {
+  return numArray.reduce((sum, num) => sum + num);
+}
+
+function square(num) {
+  return num ** 2;
+} */
+
+
+// my shortened solution
+function sumSquareDifference(count) {
+  let numbers = [];
+
+  for (let counter = 1; counter <= count; counter += 1) {
+    numbers.push(counter)
+  }
+
+  let diffNum1 = (numbers.reduce((sum, num) => sum + num)) ** 2;
+
+  let squaredNumbers = numbers.map(num => num ** 2);
+  let diffNum2 = squaredNumbers.reduce((sum, num) => sum + num);
+  
+  return diffNum1 - diffNum2;
+}
+
+
+// ----------------------------------------------------------
+// BEST SOLUTION: BOOK
+function sumSquareDifference(count) {
+  let sum = 0;
+  let sumOfSquares = 0;
+  
+  for (let number = 1; number <= count; number += 1) {
+    sum += number; 
+    sumOfSquares += Math.pow(number, 2);
+  }
+  return Math.pow(sum, 2) - sumOfSquares;
+}
 
 // E: test cases
+console.log(sumSquareDifference(3));      // 22 --> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+console.log(sumSquareDifference(10));     // 2640
+console.log(sumSquareDifference(1));      // 0
+console.log(sumSquareDifference(100));    // 25164150
--- small_problems/medium2/7.js
+++ small_problems/medium2/7.js
@@ -1,15 +1,55 @@
 /**
  * P---------------------
- * input:
- * output:
+ * input: func (array)
+ * output: same array
  * rules:
- * -
+ * - function that sorts an array using the bubble sort algorithm.
+ * - sorts in place
+ * - array has at least 2 elements
+ * - bubble sort: 
+ * We can stop iterating the first time we make a pass through the array 
+ *  without making any swaps because this means that the entire array is sorted.
  * D------------------
- * -
+ * - array
  * A-----------------
- * -
- * -
- * 
+ * - func (array)
+ * REPEAT
+ * - declare @swapped and set it to @false
+ * - start loop: iterate from @idx to Upto @array.length 
+ *    - IF array[idx] > array[idx + 1],
+ *        - /then swap them & remember something changed/
+ *        - swap(A[idx], A[idx + 1])
+ *        - set @swapped to true
+ *     - end IF
+ *  - end loop
+ * - UNTIL not swapped
+ * - return array
 */
 
+function bubbleSort(arr) {
+  while (true) {
+    let swapped = false;
+    for (let idx = 0; idx < arr.length; idx += 1) {
+      if (arr[idx] > arr[idx + 1]) {
+        [arr[idx], arr[idx + 1]] = [arr[idx + 1], arr[idx]];
+        swapped = true;
+      }
+    }
+    if (!swapped) break;
+  }
+
+  return arr;
+}
+
 // E: test cases
+let array1 = [5, 3];
+bubbleSort(array1);
+console.log(array1);    // [3, 5]
+
+let array2 = [6, 2, 7, 1, 4];
+bubbleSort(array2);
+console.log(array2);    // [1, 2, 4, 6, 7]
+
+let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
+bubbleSort(array3);
+console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
